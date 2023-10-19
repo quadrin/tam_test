ad0c4199b38a02ea8ef0281abd1b2ff1acaf7a52
Alex Kesin <alexkesin@gmail.com>
2023-10-19 08:23:20 -0400

    Update gero_analysis_final.py

--- gero_analysis_final.py
+++ gero_analysis_final.py
@@ -45,58 +45,102 @@ def reduction_to_penetration(reduction):
 
     return penetration
 
-def adoption_multiplier(t, max_rate, peak_time, slowed_growth_duration):
-    if not (0 < max_rate < 1):
-        raise ValueError("max_rate must be between 0 and 1")
-    s = np.log(max_rate / (1 - max_rate))
-    t_50 = default_start_year + default_patent_duration / 2
-    k = s / (default_peak_time + slowed_growth_duration - 2 * t_50)
-    adopt_mult = expit(k * (t - t_50))
+def savings_to_population(savings, max_population, steepness):
+    # Parameters of the sigmoid function
+    max_savings = max_population  # Assume that max savings correspond to max population
+
+    # Sigmoid function
+    population = max_population / (1 + np.exp(-steepness * (savings - max_savings / 2)))
+
+    return population
+
+def adoption_multiplier(t, max_rate, peak_time):
+    if t <= peak_time:
+        return max_rate * (1 - np.exp(-t / peak_time))
+    else:
+        return max_rate
 
     return max(0, adopt_mult)  # Ensure that the adoption multiplier is not less than 0
 
-def calculate_spending(start_year, year, state, condition, adjusted_peak_time, adjusted_slow_growth_duration, reduction_rate, total_older_adults, discounts, annual_inflation, spending_data, total_spending_per_condition, total_spending_all_conditions):     
-    # Get the spending data for the given state and condition
-    original_spending = total_spending_per_condition[condition]
-    print(f"Original spending for {condition}: {original_spending}")
+def calculate_spending(start_year, year, state, condition, adjusted_peak_time, adjusted_slow_growth_duration, reduction_rate, total_older_adults, discounts, annual_inflation, spending_data, total_spending_per_condition, total_spending_all_conditions, population_data):     
+    year_since = year - start_year
+    
+    # Get the spending data for the given condition across all states
+    original_spending = spending_data.loc[:, condition].sum()
 
-    reduction_rate = 1 - reduction_rate
+    # Get the population data for the given condition across all states
+    population = population_data.loc[:, condition].sum()
 
-    # Convert reduction_rate to penetration_rate
+    # Convert reduction_rate to penetration_rate using a logistic function
     penetration_rate = reduction_to_penetration(reduction_rate)
-    print(f"Penetration rate for {condition}: {penetration_rate}")
 
     # Calculate the adoption multiplier for the given year
-    adopt_mult = adoption_multiplier(year, penetration_rate, adjusted_peak_time, adjusted_slow_growth_duration)
-    print(f"Adoption multiplier for {condition} in year {year}: {adopt_mult}")
+    adopt_mult = adoption_multiplier(year_since, penetration_rate, adjusted_peak_time)
 
-    # Calculate the new spending based on the spending data, adoption multiplier, and penetration rate
-    new_spending = original_spending * adopt_mult 
+    # Calculate the new spending based on the adjusted original spending, adoption multiplier, and penetration rate
+    new_spending = original_spending * (1 - adopt_mult)
     print(f"New spending for {condition} in year {year}: {new_spending}")
 
     # Adjust for inflation
     inflation_adjustment = (1 + annual_inflation) ** (year - start_year)
-    new_spending *= inflation_adjustment
+    new_spending /= inflation_adjustment
     print(f"New spending for {condition} in year {year} after inflation adjustment: {new_spending}")
 
-    original_spending *= inflation_adjustment
+    original_spending /= inflation_adjustment
 
-    # Calculate the difference between the original spending and the new spending
-    spending_difference = original_spending - new_spending
-    print(f"Spending difference for {condition} in year {year}: {spending_difference}")
+    # Calculate the savings for the current year
+    savings = original_spending - new_spending
 
-    return spending_difference
+    # Calculate the savings per person
+    savings_per_person = savings / population
 
-def calculate_consumer_revenue(start_year, year, max_rate, peak_time, slow_growth_duration, total_older_adults, list_price, annual_inflation):
-    # Calculate the adoption multiplier for the given year
-    adopt_mult = adoption_multiplier(year, max_rate, peak_time, slow_growth_duration)
+    # Calculate the total savings for the population that adopts the intervention
+    total_savings = savings_per_person * population * adopt_mult
 
-    # Calculate the consumer revenue based on list price, adoption multiplier, and consumer penetration rate
-    consumer_revenue = total_older_adults * adopt_mult * list_price * max_rate
+    return total_savings
+
+def calculate_spending_without_multiplier(start_year, year, state, condition, adjusted_peak_time, adjusted_slow_growth_duration, reduction_rate, total_older_adults, discounts, annual_inflation, spending_data, total_spending_per_condition, total_spending_all_conditions, population_data):     
+    year_since = year - start_year
+    
+    # Get the spending data for the given state and condition
+    original_spending = spending_data.loc[spending_data['State'] == state, condition].values[0]
+
+    # Get the population data for the given state and condition
+    population = population_data.loc[population_data['State'] == state, condition].values[0]
+
+    # Convert reduction_rate to penetration_rate using a logistic function
+    penetration_rate = reduction_to_penetration(reduction_rate)
+
+    # Calculate the new spending based on the adjusted original spending, adoption multiplier, and penetration rate
+    new_spending = original_spending * (1 - penetration_rate)
 
     # Adjust for inflation
     inflation_adjustment = (1 + annual_inflation) ** (year - start_year)
-    consumer_revenue *= inflation_adjustment
+    new_spending /= inflation_adjustment
+
+    original_spending /= inflation_adjustment
+
+    # Calculate the savings for the current year
+    savings = original_spending - new_spending
+
+    # Calculate the savings per person
+    savings_per_person = savings / population
+
+    # Calculate the total savings for the population that adopts the intervention
+    total_savings = savings_per_person * population
+
+    return total_savings
+
+def calculate_consumer_revenue(start_year, year, penetration_rate, peak_time, total_older_adults, list_price, annual_inflation):
+    # Calculate the adoption multiplier for the given year
+    adopt_mult = adoption_multiplier(year - start_year, penetration_rate, peak_time)
+
+    # Calculate the consumer revenue based on list price, adoption multiplier, and consumer penetration rate
+    consumer_revenue = total_older_adults * adopt_mult * list_price
+
+    # Adjust for inflation
+    inflation_adjustment = (1 + annual_inflation) ** (year - start_year)
+    consumer_revenue /= inflation_adjustment
 
     return consumer_revenue
 
@@ -113,7 +157,7 @@ def main():
     with sliders_col:
         plot_size = st.slider('Plot Size', min_value=0.1, max_value=15.0, value=5.0, key='plot_size')
         plot_total = st.checkbox('Plot Total Spending', value=True, key='plot_total')
-        diseases = st.multiselect('Diseases', options=list_of_diseases, default=[list_of_diseases[0]], key='diseases')
+        diseases = st.multiselect('Diseases', options=list_of_diseases, default=list_of_diseases, key='diseases')
         reduction_rate = st.number_input('Spending Reduction Rate (for payers)', min_value=0.0, max_value=1.0, value=0.2, key='reduction_rate')
         consumer_percentage = st.slider('Consumer Revenue as Percentage of Total Revenue', min_value=0.0, max_value=1.0, value=0.5, key='percentage')
         spending_percentage = 1 - consumer_percentage
@@ -133,8 +177,8 @@ def main():
     extended_years = list(range(start_year, end_year + 1))
 
     # Adjusted parameters
-    adjusted_peak_time = peak_time - 2
-    adjusted_slow_growth_duration = slow_growth_duration + 2
+    adjusted_peak_time = peak_time 
+    adjusted_slow_growth_duration = slow_growth_duration 
 
     # Load the data
     data_path = '/Users/alexkesin/adjusted_spending_data.csv'
@@ -159,21 +203,33 @@ def main():
     # Calculate the mean of each column (condition)
     reduction_percentages = spending_data.select_dtypes(include=[np.number]).mean()
 
+    # Load the population data
+    population_data = pd.read_csv('disease_populations.csv')
+
     # Normalize the reduction percentages to be between 0 and 1, and add a small constant
     epsilon = 1e-6  # small constant
     max_value = max(reduction_percentages)
     reduction_percentages = {k: epsilon + (v / max_value) * (1 - 2*epsilon) for k, v in reduction_percentages.items()}
 
     # Calculate the final consumer revenue using the selected consumer penetration rate
-    final_consumer_revenue = [calculate_consumer_revenue(start_year, year, penetration_rate, peak_time, slow_growth_duration, total_older_adults, list_price, annual_inflation) for year in extended_years]
+    final_consumer_revenue = [calculate_consumer_revenue(start_year, year, penetration_rate, peak_time, total_older_adults, list_price, annual_inflation) for year in extended_years]
 
     # Calculate total revenues, total spendings, and total combined_revenue for all diseases
-    total_spendings = [sum(calculate_spending(start_year, year, state, condition, adjusted_peak_time, adjusted_slow_growth_duration, reduction_rate, total_older_adults, discounts, annual_inflation, spending_data, total_spending_per_condition, total_spending_all_conditions) 
-                            for state in spending_data['State'] 
-                            for condition in diseases) 
-                            for year in extended_years]
-    total_combined_revenue = [final_consumer_revenue[i] * consumer_percentage + total_spendings[i] * (1 - consumer_percentage) for i in range(len(extended_years))]
-   
+    total_spendings = []
+    for year in extended_years:
+        yearly_spending = 0
+        for state in spending_data['State']:
+            for condition in diseases:
+                spending = calculate_spending(start_year, year, state, condition, adjusted_peak_time, adjusted_slow_growth_duration, reduction_rate, total_older_adults, discounts, annual_inflation, spending_data, total_spending_per_condition, total_spending_all_conditions, population_data)
+                yearly_spending += spending
+        total_spendings.append(yearly_spending)
+
+    
+    total_combined_revenue = []
+    for i in range(len(extended_years)):
+        total_revenue = consumer_percentage * final_consumer_revenue[i] + spending_percentage * total_spendings[i]
+        total_combined_revenue.append(total_revenue)
+    
     # Create a color map
     color_map = {disease: color for disease, color in zip(list_of_diseases, sns.color_palette("hls", len(list_of_diseases)))}
 
@@ -206,39 +262,38 @@ def main():
     plots_col.markdown(plot_to_html(fig, plot_size), unsafe_allow_html=True)
 
     # Calculate the Medicare spending
+    # Create a figure with two subplots side by side
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 7.5))  # Use a fixed size for the plot
+
+    # Plot individual spendings on the first subplot
     for condition in diseases:
-        spendings = []
+        individual_spendings = []
         for year in extended_years:
-            # Calculate the spending for the current year
-            spending = total_spending_per_condition[condition]
-            spendings.append(spending)
-        spend_in_billion = [s / 1e9 for s in spendings]  # Convert spending to billions
-        ax1.plot(extended_years, spend_in_billion, label=f'Condition: {condition}')
-        if plot_total:
-            total_spend_in_billion = [s / 1e9 for s in total_spendings]  # Convert total spending to billions
-            ax1.plot(extended_years, total_spend_in_billion, label='Total DiseaseSpending', color='black', linestyle='--')
-        ax2.plot(extended_years, spend_in_billion, label=f'Condition: {condition}')
+            yearly_spending = 0
+            for state in spending_data['State']:
+                spending = calculate_spending(start_year, year, state, condition, adjusted_peak_time, adjusted_slow_growth_duration, reduction_rate, total_older_adults, discounts, annual_inflation, spending_data, total_spending_per_condition, total_spending_all_conditions, population_data)
+                yearly_spending += spending
+            individual_spendings.append(yearly_spending / 1e9)  # Convert to billions
+        ax1.plot(extended_years, individual_spendings, label=f'Condition: {condition}')
 
-    if plot_total:
-        total_spend_in_billion = [s / 1e9 for s in total_spendings]  # Convert total spending to billions
-        ax1.plot(extended_years, total_spend_in_billion, label='Total Disease Spending', color='black', linestyle='--')
-        ax2.plot(extended_years, total_spend_in_billion, label='Total Disease Spending', color='black', linestyle='--')
-
-    ax1.set_title('Potential Medicare Spending Over Time for An Aging Drug (Log Scale)', fontsize=20)
+    ax1.set_title('Individual Medicare Spending Over Time for An Aging Drug', fontsize=20)
     ax1.set_xlabel('Year', fontsize=15)
-    ax1.set_ylabel('Spending ($ billions)', fontsize=15)  # Adjust y-axis label
-    ax1.set_yscale('log')  # Make y-axis logarithmic
-    ax1.legend(prop={'size': 6}, bbox_to_anchor=(1.05, 1), loc='upper left')  # Move legend to the right
+    ax1.set_ylabel('Individual Spending ($ billions)', fontsize=15)
+    ax1.legend(loc='upper left')
     ax1.grid(True)
 
-    ax2.set_title('Potential Medicare Spending Over Time for An Aging Drug (Normal Scale)', fontsize=20)
+    # Plot total spending on the second subplot
+    total_spendings = [spending / 1e9 for spending in total_spendings]
+    ax2.plot(extended_years, total_spendings, label='Total Disease Spending', color='black', linestyle='--')
+    ax2.set_title('Total Medicare Spending Over Time for An Aging Drug', fontsize=20)
     ax2.set_xlabel('Year', fontsize=15)
-    ax2.set_ylabel('Spending ($ billions)', fontsize=15)  # Adjust y-axis label
-    ax2.legend(prop={'size': 6}, bbox_to_anchor=(1.05, 1), loc='upper left')  # Move legend to the right
+    ax2.set_ylabel('Total Spending ($ billions)', fontsize=15)
+    ax2.legend(loc='upper left')
     ax2.grid(True)
 
-    fig.tight_layout()  # Adjust the layout of the plot
+    plt.tight_layout()
+    plt.show()
+
     buf = BytesIO()
     fig.savefig(buf, format="PNG", dpi=300)  # Increase DPI for better resolution
     buf.seek(0)
@@ -246,7 +301,7 @@ def main():
 
     # Create a combined revenue figure with two subplots side by side
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 7.5))  # Use a fixed size for the plot
-
+    
     # Plot the combined revenue on the first subplot (logarithmic scale)
     ax1.set_title('Total Revenue Over Time for An Aging Drug (Log Scale)', fontsize=20)
     ax1.set_xlabel('Year', fontsize=15)
